510033fbea9f36a246990991e80c9af71283e900
Estevão Lucas <estevao.lucas@gmail.com>
2016-05-03 14:29:06 -0300

    removed some admin menu itens (posts, pages, comments)

--- wp-content/themes/clubinho/functions.php
+++ wp-content/themes/clubinho/functions.php
@@ -3,5 +3,11 @@
 include(TEMPLATEPATH . '/inc/acf-repeater/acf-repeater.php');
 
 add_action('after_setup_theme', function() {
-  // add_theme_support('post-thumbnails', ['event', 'post']);
+
+});
+
+add_action('admin_menu', function() {
+  remove_menu_page('edit.php');
+  remove_menu_page('edit.php?post_type=page');
+  remove_menu_page('edit-comments.php');
 });
